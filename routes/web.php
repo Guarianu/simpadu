d6138bd85c3d935035c687e2824909bc05547d12
Guarianu <irfansban69@gmail.com>
2025-06-02 17:11:28 +0800

    data mahaiswa

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MahasiswaController;
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+// Route::get('/', function () {
+    //return view('dashboard');
+//});
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+   
